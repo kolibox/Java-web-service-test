aa44bdb49f6dda3fbaf7de6804cb15d4484f66d9
kolibomber <kolibomber@gmail.com>
2020-06-21 10:22:58 +0300

    2nd

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello Docker World123";
+		return "Hello Docker World new test";
 	}
 
 	public static void main(String[] args) {
